1ee5e2613cf0c8d249bb38e8d6bb74bd54f14632
pledgedavis <pledgedavis14@gmail.com>
2019-12-31 22:14:05 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,50 @@
+class Person 
+  def name=(new_name)
+   @name = new_name
+ end
+ def name 
+   @name
+ end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
